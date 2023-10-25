11f2f80170976eaa1d7185307f17fc2115ad6408
diannjr <dianasari25jr@gmail.com>
2023-10-25 19:28:43 +0700

    Latihan Flutter 2

---
+++ README.md
@@ -0,0 +1,3 @@
+# latihan_flutter2
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Latihan Flutter - Diana Sari")),
+        body: Container(
+            color: Colors.black26,
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Container(
+                  height: 200,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  color: Colors.white,
+                  width: double.infinity,
+                  child: Text(
+                    "Belajar Flutter Diana Sari - STTB",
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                IntrinsicHeight(
+                  child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Expanded(
+                            child: Container(
+                          color: Colors.orange,
+                          height: 150,
+                        )),
+                        Expanded(
+                            child: Container(
+                          color: Colors.green,
+                        ))
+                      ]),
+                ),
+              ],
+            )),
+      ),
+    );
+  }
+}
